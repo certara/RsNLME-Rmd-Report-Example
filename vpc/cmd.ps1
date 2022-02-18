34dd0325941166c8d1e311c02db50d2b567ada94
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2022-02-18 09:23:14 -0800

    remove run files

--- vpc/cmd.ps1
+++
@@ -1,8 +0,0 @@
-
-$INSTALLDIR="C:\Program Files\Certara\NLME_Engine"
-if($INSTALLDIR -eq "" -or $INSTALLDIR -eq $null)
-{
-  "Installation directory is not specified"
-  exit 1
-}
-powershell -noninteractive -executionpolicy remotesigned -File $INSTALLDIR\generic_run.ps1 none $INSTALLDIR $shared_directory C:\Users\jcraig\Documents\GitHub\RsNLME-Rmd-Report-Example\vpc C:\Users\jcraig\Documents\GitHub\RsNLME-Rmd-Report-Example\vpc\jobControlFile.txt 1 WorkFlow
